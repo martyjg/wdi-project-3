d09af8f46803aa52c46c94f4a4473df846c10876
odholden <odholden@gmail.com>
2015-11-23 12:14:19 +0000

    added jwt authentication stuff

--- api/app.js
+++ api/app.js
@@ -1,7 +1,6 @@
 // Requiring our packages
 
 var express        = require('express');
-var app            = express();
 var mongoose       = require('mongoose');
 var passport       = require('passport');
 var flash          = require('connect-flash');
@@ -11,24 +10,45 @@ var cookieParser   = require('cookie-parser');
 var bodyParser     = require('body-parser');
 var session        = require('express-session');
 var methodOverride = require('method-override'); 
+var jwt            = require('jsonwebtoken');
+var expressJWT     = require('express-jwt');
+
+var config         = require('./config/config');
+var User           = require('./models/user');
+var secret         = require('./config/config').secret;
+
+var app            = express();
 
 // Setup the Database
 
-var databaseURL = 'mongodb://localhost/viberate'
-mongoose.connect(databaseURL); 
+mongoose.connect(config.database);
 
-// Setup Middleware 
+
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
 
 app.use(morgan('dev')); 
 app.use(cookieParser());
 app.use(bodyParser()); 
 app.use(ejsLayouts);
 app.use(express.static(__dirname + '/public'));
-app.use(session({ secret: 'viberate-password' }));
 app.use(passport.initialize());
 app.use(passport.session()); 
 app.use(flash());
-// have left out method overwrite stuff
+
+app.use('/api', expressJWT({ secret: secret })
+  .unless({
+    path: [
+      { url: '/api/login', methods: ['POST'] },
+      { url: '/api/register', methods: ['POST'] }
+    ]
+  })
+);
 
 
 // Express settings
@@ -36,14 +56,16 @@ app.use(flash());
 app.set('view engine', 'ejs');
 app.set("views", __dirname + "/views");
 
-// require('./config/passport')(passport);
-
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    return res.status(401).json({message: 'Unauthorized request.'});
+  }
+  next();
+});
 
 
 var routes = require(__dirname + "/config/routes");
 app.use(routes);
-
-
 
 app.listen(3000);
 console.log("Heard loud and clear")
---
+++ api/config/secrets.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'secret': 'viberateisthefuture#team_moji',
+  'database': 'mongodb://localhost:27017/viberate'
+};

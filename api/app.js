e3ce958608491712603968f91533409c214cd044
JakeyPedz <j.willisfleming@gmail.com>
2015-11-23 09:56:28 +0000

    Installed and acquired all the packages

--- api/app.js
+++ api/app.js
@@ -1,1 +1,21 @@
-// I made this file all by myself
+// Requiring our packages
+
+var express        = require('express');
+var app            = express();
+var mongoose       = require('mongoose');
+var passport       = require('passport');
+var flash          = require('connect-flash');
+var ejsLayouts     = require("express-ejs-layouts");
+var morgan         = require('morgan');
+var cookieParser   = require('cookie-parser');
+var bodyParser     = require('body-parser');
+var session        = require('express-session');
+var methodOverride = require('method-override'); 
+
+// Set up the Database
+
+var databaseURL = 'mongodb://localhost/viberate'
+mongoose.connect(databaseURL); 
+
+app.listen(3000);
+console.log("Heard loud and clear")

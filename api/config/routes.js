8caa03c09510d0bc13ec5dc477e9a11e7ebc8c37
martyjg <martinjosephgormley@gmail.com>
2015-11-23 11:45:29 +0000

    Wrote users and controllers route handlers and exproted them. Unable to test without working controllers

--- api/config/routes.js
+++ api/config/routes.js
@@ -2,7 +2,33 @@ var express           = require('express');
 var router            = express.Router();
 var passport          = require("passport");
 var usersController   = require('../controllers/usersController');
+var authenticationsController = require('../controllers/authenticationsController');
 
-router.route("/users")
-  .get(usersController.indexUsers)
+router.post('/login', authenticationsController.login);
+router.post('/register', authenticationsController.register);
+
+router.route('/users/:id')
+  .get(usersController.userShow)
+  .patch(usersController.usersUpdate)
+  .delete(usersController.usersDelete)
+
+router.route('/users/:id/edit')
+  .get(usersController.usersEdit)
+
+router.route('/groups')
+  .get(groupsController.groupsIndex)
+  .post(groupsController.groupsCreate)
+
+router.route('/groups/:id')
+  .get(groupsController.groupsShow)
+  .patch(groupsController.groupsUpdate)
+  .delete(groupsController.groupsDelete)
+
+router.route('/groups/new')
+  .get(groupsController.groupsNew)
+
+router.route('/users/:id/edit')
+  .get(groupsController.groupsEdit)
+
+module.exports = router
 

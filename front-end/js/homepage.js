6281d0381501004955f02ebaa597ea0b97d521ab
martyjg <martinjosephgormley@gmail.com>
2015-11-24 16:53:07 +0000

    Added a create new group card and also displayed all existing groups that the user is part on the user homepage. Used materialize reveal card

--- front-end/js/homepage.js
+++ front-end/js/homepage.js
@@ -11,9 +11,28 @@ function displayGroups(res) {
   for (var i=0; i < groups.length; i++) {
     var id = groups[i]._id;
     $('.homepage').append(
-      "<div class='col s12 m6 l4'><div class='card'><div class='card-content'>" + groups[i].description + "</div><div class='card-action'><a href='/' id='" + id + "'>view group</a></div></div></div>"
+      // "<div class='col s12 m6 l4'><div class='card'><div class='card-content'>" + groups[i].description + "</div><div class='card-action'><a href='/' id='" + id + "'>view group</a></div></div></div>"
 
       // "<div class='card-image waves-effect waves-block waves-light col s12 m6 l4'><img class='activator' src='images/office.jpg'></div><div class='card-content'>< span class='card-title activator grey-text text-darken-4'>" + groups[i].name + "<i class='material-icons right'>more_vert</i></span><p><a href='http://localhost:3000/api/groups/'" + groups[i]._id + ">View Group</a></p></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>" + groups[i].name + "<i class='material-icons right'>close</i></span><p>" + groups[i].description + "</p></div>"
+
+      '<div class="col s12 m6 l4">' +
+      '<div class="card">' +
+          '<div class="card-image waves-effect waves-block waves-light">' +
+            '<img class="activator" src="http://materializecss.com/images/office.jpg">' +
+          '</div>' +
+          '<div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + groups[i].name + '<i class="material-icons right">more_vert</i></span>' +
+            '<p><a href="/" id="' + id + '"></a></p>' +
+          '</div>' +
+          '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + groups[i].name + '<i class="material-icons right">close</i></span>' +
+            '<p>' + groups[i].description + '</p>' +
+          '</div>' +
+        '</div>' +
+      '</div>'
+
+
+
     );
     $("#" + id).on("click", showGroupPage(id));
   }

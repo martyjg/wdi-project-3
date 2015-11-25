01c8fcb1afad236e0172c85b1e20f2081af9e9d8
martyjg <martinjosephgormley@gmail.com>
2015-11-25 17:54:12 +0000

    Added typeahead.js, no bugs on app

--- front-end/js/login-register.js
+++ front-end/js/login-register.js
@@ -8,29 +8,6 @@ function init() {
   $(".logout-link").on("click", logout);
   $(".login-link, .register-link, .groups-link, .members-link, .newgroup-link, .newpoll-link, .newmember-link").on("click", showPage);
 
-  substringMatcher();
-
-  var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-  ];
-
-  $('#the-basics .typeahead').typeahead({
-    hint: true,
-    highlight: true,
-    minLength: 1
-  },
-  {
-    name: 'states',
-    source: substringMatcher(states)
-  });
-
   hideErrors();
   checkLoginState();
 }
--- front-end/js/typeahead-search.js
+++ front-end/js/typeahead-search.js
@@ -1,7 +1,5 @@
 var substringMatcher = function(strs) {
-  console.log(strs);
   return function findMatches(q, cb) {
-    console.log(q, cb);
     var matches, substringRegex;
 
     // an array that will be populated with substring matches
@@ -19,6 +17,34 @@ var substringMatcher = function(strs) {
     });
 
     cb(matches);
+    
   };
 };
 
+// // substringMatcher();
+
+// // var method = "get";
+// // var url = "http://localhost:3000/api/users";
+// // var users = ajaxRequest(method, url, null);
+// // console.log(users);
+
+// var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+//   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+//   'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+//   'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+//   'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+//   'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+//   'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+//   'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+//   'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+// ];
+
+// $('#the-basics .typeahead').typeahead({
+//   hint: true,
+//   highlight: true,
+//   minLength: 1
+// },
+// {
+//   name: 'states',
+//   source: substringMatcher(states)
+// });

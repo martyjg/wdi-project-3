bd647805abfc98c6f20b822ab3c8a5497a2242f0
martyjg <martinjosephgormley@gmail.com>
2016-01-12 12:29:38 +0000

    Switching to master branch

--- front-end/js/typeahead-search.js
+++ front-end/js/typeahead-search.js
@@ -1,23 +1,15 @@
 var substringMatcher = function(strs) {
   return function findMatches(q, cb) {
     var matches, substringRegex;
-
-    // an array that will be populated with substring matches
     matches = [];
-
-    // regex used to determine if a string contains the substring `q`
     substrRegex = new RegExp(q, 'i');
 
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
     $.each(strs, function(i, str) {
       if (substrRegex.test(str)) {
         matches.push(str);
       }
     });
-
-    cb(matches);
-    
+    cb(matches); 
   };
 };
 
@@ -25,7 +17,6 @@ function getUsersList() {
   var allUsers = getUsers().responseJSON.users.map(function(user) { 
     return user.username 
   })
-
   $('#the-basics .typeahead').typeahead({
     hint: true,
     highlight: true,
